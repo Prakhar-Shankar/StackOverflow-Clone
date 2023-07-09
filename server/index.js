ad17471d9fe05be6fa207d6d52ca9bd4dd87584e
Prakhar-Shankar <prakharshankar247@gmail.com>
2023-07-10 01:22:34 +0530

    Issue: bad auth, although my password and username are correct

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ import express from 'express'
 import mongoose from 'mongoose'
 import cors from 'cors'
 
-const app = express;
+const app = express();
 app.use(express.json({limit:"30mb", extended:true}))
 
 // So, json hamne use kia kyuki we will receive everything in jason format, limit 30mb ka set kia which can be extended
@@ -15,3 +15,9 @@ app.get('/', (req, res)=> {
 })
 
 const PORT = process.env.PORT || 5000
+
+const CONNECTION_URL = "mongodb+srv://prakharshankar247:mercury29@stack-overflow-clone.otoaqo3.mongodb.net/?retryWrites=true&w=majority"
+
+mongoose.connect(CONNECTION_URL, {useNewUrlParser: true, useUnifiedTopology: true})
+    .then(() =>app.listen(PORT, () => {console.log(`server is running on ${PORT}`)}))
+    .catch((err) =>  console.log(err.message))
