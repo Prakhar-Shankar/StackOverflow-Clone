ecb751114be61421792f2ab1d2e56d15df8227f4
Prakhar-Shankar <prakharshankar247@gmail.com>
2023-07-11 02:27:07 +0530

    Completed the back-end part of authentication

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 import express from 'express'
 import mongoose from 'mongoose'
 import cors from 'cors'
+import userRouter from './routes/users.js'
 
 const app = express();
 app.use(express.json({limit:"30mb", extended:true}))
@@ -14,10 +15,12 @@ app.get('/', (req, res)=> {
     res.send("This is a stack overflow clone API")
 })
 
+app.use('/user', userRouter)
+
 const PORT = process.env.PORT || 5000
 
 const CONNECTION_URL = "mongodb+srv://prakharshankar247:glyYzZ3lEFvz8ZQf@stack-overflow-clone.otoaqo3.mongodb.net/?retryWrites=true&w=majority"
 
 mongoose.connect(CONNECTION_URL, {useNewUrlParser: true, useUnifiedTopology: true})
-    .then(() =>app.listen(PORT, () => {console.log(`server is running on ${PORT}`)}))
+    .then(() =>app.listen(PORT, () => {console.log(`server is running on port ${PORT}`)}))
     .catch((err) =>  console.log(err.message))
