34e7efcc71b3d80c31403d22a02268ff1bf9edc9
Prakhar-Shankar <prakharshankar247@gmail.com>
2023-07-15 03:47:39 +0530

    logout and login function

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -7,8 +7,8 @@ export const signup = async (req, res) => {
     const {name, email, password} = req.body; 
     // so we need name, email and pass in sign up page therefore we are structuring them here\
     try{
-        const existingUser = await users.findOne({email});  //check if the user already exists or not 
-        if(existingUser){
+        const existinguser = await users.findOne({email});  //check if the user already exists or not 
+        if(existinguser){
             return res.status(404).json({message:"User already exist."})//if user already exists this mssg will be shown
         }
         const hashedPassword = await bcrypt.hash(password, 12)
@@ -23,16 +23,16 @@ export const signup = async (req, res) => {
 export const login = async (req, res) => {
     const {email, password} = req.body;
     try {
-        const existingUser = await users.findOne({email});
-        if(!existingUser){
+        const existinguser = await users.findOne({email});
+        if(!existinguser){
             return res.status(404).json({message:"User don't exist."})
         }
-        const isPasswordCrt = await bcrypt.compare(password, existingUser.password)
+        const isPasswordCrt = await bcrypt.compare(password, existinguser.password)
         if(!isPasswordCrt){
             return res.status(400).json({message: "Invalid credentials"})
         }
-        const token = jwt.sign({email: newUser.email, id: newUser._id}, "test" , {expiresIn : '1h'});
-        res.status(200).json({result: newUser, token})
+        const token = jwt.sign({email: existinguser.email, id: existinguser._id}, "test" , {expiresIn : '1h'});
+        res.status(200).json({result: existinguser, token})
     } catch (error) {
         res.status(500).json("Something went wrong...")
     } 
